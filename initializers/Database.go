ffada36b3e4de4fe9dd4c015d41a8a25acc73e98
Shuwais Soudager <shuwais2003@protonmail.com>
2025-10-02 21:38:02 +0530

    Init: for .env and postgres Database

---
+++ initializers/Database.go
@@ -0,0 +1,21 @@
+// Package initializers:Connect2DB is used for conneting to the DB
+package initializers
+
+import (
+	"log"
+	"os"
+
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+var DB *gorm.DB
+
+func Connect2DB() {
+	var err error
+	dsn := os.Getenv("DB_URL")
+	DB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		log.Fatal("ERROR: GORM : Failed to Connect to the DB")
+	}
+}
---
+++ initializers/loadEnvVariables.go
@@ -0,0 +1,14 @@
+package initializers
+
+import (
+	"log"
+
+	"github.com/joho/godotenv"
+)
+
+func LoadEnvVariables() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("ERROR: DOTENV : Loading Failed")
+	}
+}
